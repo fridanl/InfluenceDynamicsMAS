ef2bde921a8fd52e756e4aee8272b2d751ccd24a
fridanl <frida.nl@hotmail.com>
2026-05-29 16:39:06 +0200

    Update test-run.sh

--- test-run.sh
+++ test-run.sh
@@ -20,20 +20,28 @@ uv sync
 SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 
 # First run
-uv run run.py 
-    --model_name llama-3.1-8b \
-    --dataset sarcasm \
-    --dataset_path data/sarc/sarcasm.csv \
-    --repetition 10 \
-    --round 1 \
-    --slurm_output "${SLURM_OUTPUT_FILE}" \
-    -limit 100
+# uv run run.py 
+#     --model_name llama-3.1-8b \
+#     --dataset sarcasm \
+#     --dataset_path data/sarc/sarcasm.csv \
+#     --repetition 10 \
+#     --round 1 \
+#     --slurm_output "${SLURM_OUTPUT_FILE}" \
+#     -limit 100
 
-uv run run.py 
-    --model_name gemma-3-4b \
+# uv run run.py 
+#     --model_name gemma-3-4b \
+#     --dataset sarcasm \
+#     --dataset_path data/sarc/sarcasm.csv \
+#     --repetition 10 \
+#     --round 1 \
+#     --slurm_output "${SLURM_OUTPUT_FILE}" \
+#     -limit 100
+
+
+uv run -m src.make_second_round_input \
+    --dataset sarcasm
+
+uv run -m src.make_second_round_input \
     --dataset sarcasm \
-    --dataset_path data/sarc/sarcasm.csv \
-    --repetition 10 \
-    --round 1 \
-    --slurm_output "${SLURM_OUTPUT_FILE}" \
-    -limit 100
+    --self_interaction
