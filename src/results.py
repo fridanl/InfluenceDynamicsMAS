b908591ad04bff55101addd345662c2175d8cfd7
fridanl <98814862+fridanl@users.noreply.github.com>
2026-05-31 10:37:24 +0200

    Update default base path for results

--- src/results.py
+++ src/results.py
@@ -407,7 +407,7 @@ def main(args):
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument('--base_path',
-                default='/home/results',
+                default='/home/InfluenceDynamicsMAS/results',
                 help='base path for results.')
     ap.add_argument("--dataset", 
                     help="Specify name of dataset",
